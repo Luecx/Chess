5bdee61fc209ff446986ea063cc7a241d5ce5ee9
Finn Eggers <web@finneggers.de>
2020-05-04 07:59:25 +0200

    quicksort movelists

--- engines/README.md
+++ engines/README.md
@@ -1,6 +1,6 @@
 
 
-## Elo-Ratings:
+# Elo-Ratings:
 
 
 ### Waldi
@@ -12,8 +12,8 @@ Waldi 3  | 1700-1800
 Waldi 4  | 1720-1820
 Waldi 5  | 1800-1850  
 Waldi 6  | 1950
-
-
-
+Waldi 7  | 2165
 
 ----
+
+
--- src/board/moves/MoveList.java
+++ src/board/moves/MoveList.java
@@ -143,6 +143,47 @@ public class MoveList implements Iterable<Move>, List<Move> {
     }
 
     @Override
+    public void sort(Comparator<? super Move> c) {
+        quicksort(0, this.size-1);
+    }
+
+    private void quicksort(int low, int high) {
+        int i = low, j = high;
+        // Get the pivot element from the middle of the list
+        int pivot = moves[low + (high-low)/2].getOrderPriority();
+
+        // Divide into two lists
+        while (i <= j) {
+            // If the current value from the left list is smaller than the pivot
+            // element then get the next element from the left list
+            while (moves[i].getOrderPriority() < pivot) {
+                i++;
+            }
+            // If the current value from the right list is larger than the pivot
+            // element then get the next element from the right list
+            while (moves[j].getOrderPriority() > pivot) {
+                j--;
+            }
+
+            // If we have found a value in the left list which is larger than
+            // the pivot element and if we have found a value in the right list
+            // which is smaller than the pivot element then we exchange the
+            // values.
+            // As we are done we can increase i and j
+            if (i <= j) {
+                swap(i, j);
+                i++;
+                j--;
+            }
+        }
+        // Recursion
+        if (low < j)
+            quicksort(low, j);
+        if (i < high)
+            quicksort(i, high);
+    }
+
+        @Override
     public List<Move> subList(int fromIndex, int toIndex) {
         throw new RuntimeException();
     }
@@ -201,6 +242,7 @@ public class MoveList implements Iterable<Move>, List<Move> {
             }
         };
     }
+
 
     @Override
     public Object[] toArray() {
